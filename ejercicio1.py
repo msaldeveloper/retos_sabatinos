c1d65ef5b6d33f773455f49ca948c67731bb793c
msalda <msalda53@gmail.com>
2021-02-20 12:41:46 -0600

    añadiendo correcciones

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,13 +1,15 @@
+"""
+funcion que al ingresar un numero entero te devuelve 
+la longitud de ese numero 
+"""
 def contador(numero):
-    if type(numero) != int:
-        return("ingresa un numero entero no una cadena de texto")
-    elif numero>0: 
+    if type(numero) != int or numero<0:
+        return ("numero incorrecto porfavor ingresa un entero")
+    else:
         cambio=str(numero)
         contador=0
         for i in cambio:
             #print (i)
             contador+=1
-        return (f"la longitud de tu numero es \n{contador}")
-    
-    else:
-        return ("numero invalido ingresa un numero entero")
+        return (f"la longitud de tu numero es {contador}")
+
