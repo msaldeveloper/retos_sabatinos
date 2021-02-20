35586d81195989c9ba430854ceeabe512f01e883
msalda <msalda53@gmail.com>
2021-02-20 12:30:23 -0600

    aplicando restricciones

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,7 +1,13 @@
 def contador(numero):
-    cambio=str(numero)
-    contador=0
-    for i in cambio:
-        #print (i)
-        contador+=1
-    print(f"la longitud del numero es {contador}")
+    if type(numero) != int:
+        return("ingresa un numero entero no una cadena de texto")
+    elif numero>0: 
+        cambio=str(numero)
+        contador=0
+        for i in cambio:
+            #print (i)
+            contador+=1
+        return (f"la longitud de tu numero es \n{contador}")
+    
+    else:
+        return ("numero invalido ingresa un numero entero")
